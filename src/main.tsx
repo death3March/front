55ef95743da37fbb2ec9db7f275b6807e198b707
ukai <73654679+snct-ukai@users.noreply.github.com>
2024-11-18 23:55:26 +0900

    update: use WebSocket Provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./app.tsx";
+import {WebSocketProvider} from "@/features/web-socket-provide.tsx";
 
 export const router = createBrowserRouter([
 	{
@@ -15,6 +16,8 @@ export const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<RouterProvider router={router} />
+		<WebSocketProvider>
+			<RouterProvider router={router} />
+		</WebSocketProvider>
 	</StrictMode>,
 );
