977f66eefa6b00db21f539b92c058ba85fd3bb90
Bayathy <misosoup0321@icloud.com>
2024-11-20 01:45:07 +0900

    feat: 🚀 update

--- src/features/web-socket-provider.tsx
+++ src/features/web-socket-provider.tsx
@@ -16,28 +16,34 @@ export const WebSocketProvider: React.FC<WebSocketProviderProps> = ({ children }
 	const [message, setMessage] = useState<string | null>(null);
 
 	useEffect(() => {
-		ws.current = new WebSocket("ws://127.0.0.1:8080");
+		const connectWebSocket = () => {
+			ws.current = new WebSocket("ws://127.0.0.1:8080");
 
-		ws.current.onopen = () => {
-			console.log("WebSocket connection opened");
+			ws.current.onopen = () => {
+				console.log("WebSocket connection opened");
+			};
+
+			ws.current.onmessage = (event) => {
+				console.log("Received message:", event.data);
+				setMessage((prev) => (prev ? prev + event.data : event.data));
+			};
+
+			ws.current.onclose = () => {
+				console.log("WebSocket connection closed");
+				setTimeout(connectWebSocket, 1000);
+			};
+
+			ws.current.onerror = (error) => {
+				console.error("WebSocket error:", error);
+			};
 		};
 
-		ws.current.onmessage = (event) => {
-			console.log("Received message:", event.data);
-			setMessage(event.data);
-		};
-
-		ws.current.onclose = () => {
-			console.log("WebSocket connection closed");
-		};
-
-		ws.current.onerror = (error) => {
-			console.error("WebSocket error:", error);
-		};
+		connectWebSocket();
 
 		return () => {
 			if (ws.current) {
 				ws.current.close();
+				ws.current.onmessage = null;
 			}
 		};
 	}, []);
@@ -45,6 +51,7 @@ export const WebSocketProvider: React.FC<WebSocketProviderProps> = ({ children }
 	return <WebSocketContext.Provider value={{ ws: ws.current, message }}>{children}</WebSocketContext.Provider>;
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useWebSocket = (): WebSocketContextType | null => {
 	return useContext(WebSocketContext);
 };
---
+++ src/route.tsx
@@ -0,0 +1,8 @@
+import { Home } from "./features/home/pages";
+
+export const route = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+	},
+]);
