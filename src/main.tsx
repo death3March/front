a17677509af8b51b6b2e2514eeb3b4f7497250d1
Bayathy <misosoup0321@icloud.com>
2024-11-21 15:37:02 +0900

    chore: 🔨 add tanstack router and tanstack query
    ✅ Closes: 6

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,22 @@
 import "./index.css";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
 
 import { WebSocketProvider } from "@/features/web-socket-provider";
 
-import { route } from "./route";
+import { router } from "./router";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<WebSocketProvider>
-			<RouterProvider router={route} />
-		</WebSocketProvider>
+		<QueryClientProvider client={queryClient}>
+			<WebSocketProvider>
+				<RouterProvider router={router} />
+			</WebSocketProvider>
+		</QueryClientProvider>
 	</StrictMode>,
 );
