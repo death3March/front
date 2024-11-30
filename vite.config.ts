8335845c20a4c0607adb2292c3055b9931d49262
Bayathy <misosoup0321@icloud.com>
2024-12-01 01:57:39 +0900

    fix: 🐞 change config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": "/src",
-		},
-	},
+	plugins: [react(), tsconfigPaths()],
 });
