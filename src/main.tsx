599f9554fa5abcc24620d19f63a1b3a389babf06
Bayathy <misosoup0321@icloud.com>
2024-12-03 17:06:34 +0900

    feat: 🚀 add react-scan

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,13 @@
 import "./index.css";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import { WebSocketProvider } from "@/features/web-socket-provider";
-
 import { router } from "./router";
-
-const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<WebSocketProvider>
-				<RouterProvider router={router} />
-			</WebSocketProvider>
-		</QueryClientProvider>
+		<RouterProvider router={router} />
 	</StrictMode>,
 );
