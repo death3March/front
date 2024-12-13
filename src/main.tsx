f4adbecadc9be17b457528a491a675c213ae6b7f
Bayathy <misosoup0321@icloud.com>
2024-12-13 18:20:11 +0900

    fix: disable react-scan when production

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,15 @@ import { RouterProvider } from "@tanstack/react-router";
 import { Provider as JotaiProvider } from "jotai";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { scan } from "react-scan";
 
 import { router } from "./router";
+
+if (import.meta.env.DEV) {
+	scan({
+		enabled: true,
+	});
+}
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
